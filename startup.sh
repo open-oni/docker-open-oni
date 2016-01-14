ec4f16eb30636123f530eafd30efb20e80b3b26d
Greg Tunink <techgique@gmail.com>
2016-01-14 16:05:29 -0600

    Prevent lingering Apache PID file from breaking container
    If the container is shut down before Apache removes its PID file,
    Apache will fail to start when the container is started next and the
    container will close immediately after

--- startup.sh
+++ startup.sh
@@ -10,5 +10,11 @@ django-admin.py syncdb --noinput --migrate
 django-admin.py chronam_sync --skip-essays
 django-admin.py collectstatic --noinput
 
+
+# Remove any pre-existing PID file which prevents Apache from starting
+#   thus causing the container to close immediately after
+#   See: https://github.com/docker-library/php/pull/59
+rm -f /var/run/apache2/apache2.pid
+
 source /etc/apache2/envvars
 exec apache2 -D FOREGROUND
